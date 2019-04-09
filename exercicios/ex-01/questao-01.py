062cf225b62ddf3f310ec507a76e00d20491283d
Adriner Andrade <adriner_andrade@hotmail.com>
2019-04-08 22:02:37 -0300

    método de construção de matriz

--- exercicios/ex-01/questao-01.py
+++ exercicios/ex-01/questao-01.py
@@ -1,13 +1,26 @@
 import matplotlib.pyplot as plt
+from random import randint
 
 def exibir(I):
     plt.imshow(I, 'gray')
-    plt.show(block=False)
+    plt.show(block=True)
     plt.pause(0.5)
     plt.clf()
 
-matrix = [[0 for i in range(5)] for j in range(5)]
-
+def buildMatriz(size):
+	m = [[0 for x in range(size)] for y in range(size)]
+	for x in range(size):
+		for y in range(size):
+			if y == 0 or y == (size - 1):
+				m[x][y] = 1
+			elif x == 0 or x == (size - 1):
+				m[x][y] = 1
+			else:
+				ran = randint(0,1)
+				m[x][y] = 2 if ran == 1 else 0
+	return m
+	
+matrix = buildMatriz(6)
 print(matrix)
 
 exibir(matrix)
