cad59bcf9d1448c58de47981d2b2a07d0179c942
Fábio L. Fischer <feuture@hotmail.com>
2019-04-11 16:52:31 -0300

    estrutura de animação

--- exercicios/ex-01/questao-01.py
+++ exercicios/ex-01/questao-01.py
@@ -1,26 +1,41 @@
+import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from random import randint
 
-def exibir(I):
-    plt.imshow(I, 'gray')
-    plt.show(block=True)
-    plt.pause(0.5)
-    plt.clf()
+IMG_WALL_TILE = 1
+IMG_CLEAN_TILE = 0
+IMG_DIRTY_TILE = 2
+IMG_AGENT_TILE = 3
+
+fig = plt.figure()
+frameRate = 3
 
 def buildMatriz(size):
-	m = [[0 for x in range(size)] for y in range(size)]
-	for x in range(size):
-		for y in range(size):
-			if y == 0 or y == (size - 1):
-				m[x][y] = 1
-			elif x == 0 or x == (size - 1):
-				m[x][y] = 1
-			else:
-				ran = randint(0,1)
-				m[x][y] = 2 if ran == 1 else 0
-	return m
-	
+    m = [[0 for x in range(size)] for y in range(size)]
+    for x in range(size):
+        for y in range(size):
+            if y == 1 and x == 1:
+                m[x][y] = IMG_AGENT_TILE
+            elif y == 0 or y == (size - 1):
+                m[x][y] = IMG_WALL_TILE
+            elif x == 0 or x == (size - 1):
+                m[x][y] = IMG_WALL_TILE
+            else:
+                ran = randint(0, 1)
+                m[x][y] = IMG_DIRTY_TILE if ran == 1 else IMG_CLEAN_TILE
+    return m
+
 matrix = buildMatriz(6)
 print(matrix)
+cmap = matplotlib.cm.Greys
+im = plt.imshow(matrix, cmap, animated=True)
 
-exibir(matrix)
+def updatefig(*args):
+    global matrix
+    matrix = buildMatriz(6)
+    im.set_array(matrix)
+    return im,
+
+ani = animation.FuncAnimation(fig, updatefig, interval=1000/frameRate, blit=True)
+plt.show()
